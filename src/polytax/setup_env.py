88ad471270bc25bab12c7d61745d89daad81af0c
root <root@t1v-n-4a027db3-w-0.us-central1-f.c.must-318416.internal>
2021-07-30 12:33:47 +0000

    env setup

--- src/polytax/setup_env.py
+++ src/polytax/setup_env.py
@@ -1,12 +1,14 @@
 import yaml
 import os
 
-def setup_env():
+def setup_env(args):
     print("setting up env")
     stream = open("env.yaml", 'r')
     dictionary = yaml.load(stream)
     for key, value in dictionary["XRT_CONFIGS"].items():
-        print(f"export {key}={value}")
-        os.system(f"export {key}={value}")
+        print(f"{key}={value}")
+        os.environ[key] = value
     print("exported")
     os.system("unset LD_PRELOAD;")
+    os.environ["XRT_TORCH_DIST_ROOT"]=f"{args.addr}:{args.port}"
+    os.environ["XRT_MESH_SERVICE_ADDRESS"]=f"tcp://{args.addr}:{args.port}"
