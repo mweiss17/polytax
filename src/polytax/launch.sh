68fe9504e592a7aa6bc7fd31c95d4ab36d3c45b3
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-08-24 16:07:12 -0400

    updated launch scripts

--- src/polytax/launch.sh
+++ src/polytax/launch.sh
@@ -8,40 +8,40 @@ RANGE=${5:-"192.169.0.0/29"}
 echo launching $NUMNODES nodes on $NETWORK/$SUBNETWORK
 
 # Boot the root node (i.e. control plane)
-gcloud alpha compute tpus tpu-vm create "node-$ROOTID" \
-  --zone us-central1-f \
-  --network $NETWORK \
-  --subnetwork $SUBNETWORK \
-  --range $RANGE \
-  --accelerator-type v2-8 \
-  --version v2-alpha \
+#gcloud alpha compute tpus tpu-vm create "node-$ROOTID" \
+#  --zone us-central1-f \
+#  --network $NETWORK \
+#  --subnetwork $SUBNETWORK \
+#  --range $RANGE \
+#  --accelerator-type v2-8 \
+#  --version v2-alpha \
 #  --metadata=startup-script="#! /bin/bash
 #    export XRT_TPU_CONFIG='localservice;0;localhost:51011'; unset LD_PRELOAD; cd /home/root; git clone https://github.com/mweiss17/polytax.git; cd polytax; python3 -m pip install --upgrade build; cd src/polytax; python3 launch.py --rank=0 --addr=$CONTROLIP --port=2345 >> /home/root/polytax/logs.txt
 #  "
 
 # Get internal-ip for the controlling node
-CONTROLIP=$(gcloud alpha compute tpus describe node-0 --format='get(ipAddress)')
-echo $CONTROLIP
-for i in $(seq 1 $(($NUMNODES-1))); do
-  NODENAME="node-$(($i+$ROOTID))"
-  echo creating $NODENAME
+#CONTROLIP=$(gcloud alpha compute tpus describe node-0 --format='get(ipAddress)')
+#echo $CONTROLIP
+#for i in $(seq 1 $(($NUMNODES-1))); do
+#  NODENAME="node-$(($i+$ROOTID))"
+#  echo creating $NODENAME
+#
+#  # Spool up a TPU node
+#  gcloud alpha compute tpus tpu-vm create $NODENAME \
+#  --zone us-central1-f \
+#  --network $NETWORK \
+#  --subnetwork $SUBNETWORK \
+#  --range $RANGE \
+#  --accelerator-type v2-8 \
+#  --version v2-alpha \
+#  --async
+#done
 
-  # Spool up a TPU node
-  gcloud alpha compute tpus tpu-vm create $NODENAME \
-  --zone us-central1-f \
-  --network $NETWORK \
-  --subnetwork $SUBNETWORK \
-  --range $RANGE \
-  --accelerator-type v2-8 \
-  --version v2-alpha \
-  --async
-done
-
-for i in $(seq 1 $(($NUMNODES-1))); do
+for i in $(seq 0 $(($NUMNODES-1))); do
   NODENAME="node-$(($i+$ROOTID))"
   echo running startup script on $NODENAME
 
-  gcloud alpha compute tpus tpu-vm ssh $NODENAME --zone us-central1-f --command "cd ~/; git clone https://github.com/mweiss17/polytax.git; cd polytax; ./launch_tpu.sh  $i"
+  gcloud alpha compute tpus tpu-vm ssh $NODENAME --zone us-central1-f --command "cd ~/; git clone https://github.com/mweiss17/polytax.git; cd polytax; chmod 755 src/polytax/*.sh; cd src/polytax; ./launch_tpu.sh  $i"
 
 done
 
@@ -89,11 +89,11 @@ done
 #          '{}:{}'.format(client_worker.get_hostname(), remote_path),
 #          ]
 
-
-   def _build_ssh_cmd(self, remote_cmd, client_worker):
-    services = self._cluster.get_service_workers()
-    service_map = {s.get_internal_ip(): s._tpu for s in services}
-    tpu_name = service_map[client_worker.get_internal_ip()]
+#
+#   def _build_ssh_cmd(self, remote_cmd, client_worker):
+#    services = self._cluster.get_service_workers()
+#    service_map = {s.get_internal_ip(): s._tpu for s in services}
+#    tpu_name = service_map[client_worker.get_internal_ip()]
 
 
 #
--- src/polytax/launch_tpu.sh
+++ src/polytax/launch_tpu.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 rank=$1
 addr=$2
+
 echo rank: $rank, addr: $addr
 
-cd ../../
+#sudo apt install python3-venv -Y
+#python3 -m venv polytax-env
+#source polytax-env/bin/activate
+pip install --upgrade clu
+pip3 install --upgrade "cloud-tpu-profiler>=2.3.0"
+
+
 python3 -m pip install --upgrade build
-cd src/polytax
+python3 setup.py install
+
+cd ~/
+
+#pip install datasets
+
+# Install huggingface from source, editably
+git clone https://github.com/huggingface/transformers.git
+cd transformers
+pip install wheel
+python3 setup.py bdist_wheel
+pip install -e .[flax]
+
+cd ../polytax/src/polytax
+
+ln -s ~/transformers/examples/flax/language-modeling/t5_tokenizer_model.py t5_tokenizer_model.py
+ln -s ~/transformers/examples/flax/language-modeling/run_t5_mlm_flax.py run_t5_mlm_flax.py
+
+# python3 preprocess.py
+# from transformers import T5Config
+#from transformers import PreTrainedTokenizerFast
+# tokenizer = PreTrainedTokenizerFast(tokenizer_file="tokenizer.json")
+# config = T5Config(vocab_size=tokenizer.vocab_size)
+
 python3 launch.py --rank=$rank --addr=$addr --port=2345 >> logs.txt
+
+
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
