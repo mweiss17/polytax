cf3e10fd8f5aab2954f2862f69bac3d84d445051
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-08-26 18:18:25 -0400

    cleaning up launch script

--- src/polytax/launch_tpu.sh
+++ src/polytax/launch_tpu.sh
@@ -10,6 +10,8 @@ echo rank: $rank, addr: $addr
 pip3 install --upgrade clu
 pip3 install --upgrade "cloud-tpu-profiler>=2.3.0"
 pip3 install datasets
+pip3 install tbp-nightly
+pip install google-cloud
 
 python3 -m pip install --upgrade build
 pip3 install wheel
@@ -20,25 +22,40 @@ cd ~/
 #pip install datasets
 
 # Install huggingface from source, editably
-git clone https://github.com/huggingface/transformers.git
-cd transformers
+git clone https://github.com/mweiss17/transformers.git
+cd ~/transformers/
+rm pyproject.toml
 pip3 install -e .[flax]
 
 cd ~/polytax/
-python3 setup.py install
+python3 setup.py install --user
 
 cd ~/polytax/src/polytax
 
 ln -s ~/transformers/examples/flax/language-modeling/t5_tokenizer_model.py t5_tokenizer_model.py
 ln -s ~/transformers/examples/flax/language-modeling/run_t5_mlm_flax.py run_t5_mlm_flax.py
 
-# python3 preprocess.py
-# from transformers import T5Config
-#from transformers import PreTrainedTokenizerFast
-# tokenizer = PreTrainedTokenizerFast(tokenizer_file="tokenizer.json")
-# config = T5Config(vocab_size=tokenizer.vocab_size)
+export $PATH=$PATH:/home/martin/.local/bin
+unset LD_PRELOAD
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+
+python3 run_t5_mlm_flax.py \
+	--output_dir="./" \
+	--model_type="t5" \
+	--config_name="./" \
+	--tokenizer_name="./" \
+	--dataset_name="realnewslike" \
+	--max_seq_length="512" \
+	--per_device_train_batch_size="32" \
+	--per_device_eval_batch_size="32" \
+	--adafactor \
+	--learning_rate="0.005" \
+	--weight_decay="0.001" \
+	--warmup_steps="2000" \
+	--overwrite_output_dir \
+	--logging_steps="500" \
+	--save_steps="10000" \
+	--eval_steps="2500" \
+	--push_to_hub
 
 python3 launch.py --rank=$rank --addr=$addr --port=2345 >> logs.txt
-
-
-export XRT_TPU_CONFIG="localservice;0;localhost:51011"
