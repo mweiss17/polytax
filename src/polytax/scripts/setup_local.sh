bec70ef057e848a229b459c0b3ed225d76cb9d2d
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2021-10-05 19:40:15 -0400

    added back old setup_local for quicker access locally

---
+++ src/polytax/scripts/setup_local.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install some stuff
+pip3 install --upgrade clu
+pip3 install tbp-nightly
+pip3 install google-cloud
+pip3 install wandb
+pip3 install tensorboardX
+pip3 install dill
+pip3 install mesh-tensorflow
+pip3 install seqio
+
+python3 -m pip install --upgrade build
+pip3 install wheel
+python3 setup.py bdist_wheel
+
+cd ~/
+git clone https://github.com/mweiss17/transformers.git
+cd ~/transformers/
+rm pyproject.toml
+pip3 install -e .[flax]
+
+
+cd ~/
+git clone https://github.com/nasimrahaman/speedrun.git
+cd speedrun/
+python3 setup.py install
+
+cd ~/polytax/
+python3 setup.py install --user
