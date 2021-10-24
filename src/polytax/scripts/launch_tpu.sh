4dfbdd192d8c1a7c31a9d29beeddb7e2fbab602a
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-10-24 14:30:32 -0400

    fix echos

--- src/polytax/scripts/launch_tpu.sh
+++ src/polytax/scripts/launch_tpu.sh
@@ -13,7 +13,7 @@ pip3 install -e .[xla] # for some reason this fails if we python3 setup.py insta
 cd ~/polytax/src/polytax
 export WANDB_API_KEY=$(curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/wandb_api_key" -H "Metadata-Flavor: Google")
 
-echo export WANDB_API_KEY=$WANDB_API_KEY >> ~/.bashrc
+echo export WANDB_API_KEY=\"${WANDB_API_KEY}\" >> ~/.bashrc
 
 git config --global user.email "martin.clyde.weiss@gmail.com"
 git config --global user.name "Martin Weiss"
@@ -23,7 +23,7 @@ export PATH=$PATH:/home/$USER/.local/bin
 unset LD_PRELOAD
 
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
-echo export XRT_TPU_CONFIG="localservice;0;localhost:51011" >> ~/.bashrc
+echo export XRT_TPU_CONFIG=\"localservice\;0\;localhost:51011\" >> ~/.bashrc
 
 #python3 -m torch.distributed.run --nnodes=1 --nproc_per_node=1 --rdzv_id=1 --rdzv_backend=c10d --rdzv_endpoint="localhost:2379"  train.py experiments/$expname --inherit $templatename
 python3 -m torch.distributed.run --nnodes=1 --nproc_per_node=1 --rdzv_id=1 --rdzv_backend=c10d --rdzv_endpoint="localhost:2379"  train.py experiments/$expname --inherit $templatename
