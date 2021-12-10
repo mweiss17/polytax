d70aaef33f2d8eb8d43441f69bf4a328899bf837
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-12-10 14:42:32 -0500

    change syntax

--- src/polytax/train.py
+++ src/polytax/train.py
@@ -373,7 +373,8 @@ class Trainer(WandBMixin, IOMixin, BaseExperiment):
                         if isinstance(p, torch.Tensor) and p.grad is not None:
                             if device == torch.device("cpu"):
                                 print(f"detaching gradients from {p.size()}")
-                                gradients.append(p.detach().cpu())
+                                gradients.append(p.grad.cpu())
+                                # gradients.append(p.detach().cpu())
                             else:
                                 gradients.append(p.grad.data)
         print("done fetching grads")
